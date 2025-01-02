6b1f1872634be97f0536da2d9f5ac7a803af85f0
jeerikje <159952979+jeerikje@users.noreply.github.com>
2025-01-02 09:37:02 +0100

    second commit
    Odstránenie SignUpView, presunutie Cart,Deti,Muzi,Zeny,Zdravie,Zvierata do views, pridanie light a dark theme, pridanie Footer banner a Theme komponentov, doplnenie skincareView a ParfumyView

---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+export default {
+  name: "Footer"
+}
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="content">
+      <ul class="links">
+        <li><a href="#" class="icon">Facebook</a></li>
+        <li><a href="#" class="icon">Twitter</a></li>
+        <li><a href="#" class="icon">Instagram</a></li>
+        <li><a href="#" class="icon">Telefónne číslo</a></li>
+      </ul>
+      <div class="bottom">
+        <p>&copy; 2024 Michal Smatana. Vlastnoručne vyrobené. </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer{
+  background-color: #0c0c0c;
+  color: #fff;
+  padding: 2rem;
+}
+.content{
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.links{
+  display: flex;
+  justify-content: center;
+  gap: 3rem;
+  flex-wrap: wrap;
+}
+.icon{
+  color: #fff;
+  font-size: 1.5rem;
+  transition: color 0.3s ease;
+}
+.bottom{
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
